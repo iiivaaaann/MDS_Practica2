8249ad8038c450277a3eb458065b8332852c0619
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-04-04 16:44:39 +0200

    Ejercicio 6

--- ejer6.py
+++ ejer6.py
@@ -1,4 +1,5 @@
 import re
-pattern=""
+pattern="(?:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3} INFO \d+ --- )(?:\[)(\w+)(?:\])(?: \w+\.)(\w+)(?: : )(.*)"
 result=re.findall(pattern, input())
-print(result)
+for r in result:
+    print("''INFO'',''" +r[0]+"'',''"+r[1]+"'',''"+r[2]+"''")
