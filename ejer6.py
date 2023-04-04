792f9da778f524b12a2358f6b724ad3ce99815cb
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-04-04 16:53:14 +0200

    Ejercicio 6-Mejora

--- ejer6.py
+++ ejer6.py
@@ -1,5 +1,5 @@
 import re
-pattern="(?:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3} INFO \d+ --- )(?:\[)(\w+)(?:\])(?: \w+\.)(\w+)(?: : )(.*)"
+pattern="(?:\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3} INFO \d+ --- )(?:\[)(\w+)(?:\] )(?:\w+\.){1,}(\w+)(?: : )(.*)"
 result=re.findall(pattern, input())
 for r in result:
     print("''INFO'',''" +r[0]+"'',''"+r[1]+"'',''"+r[2]+"''")
