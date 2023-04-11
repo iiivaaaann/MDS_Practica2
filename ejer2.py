d2d5476fa3d8ecf740ccea6dc81557b2a38b1204
i.garcial.2020 <i.garcial.2020@alumnos.urjc.es>
2023-04-11 12:24:15 +0200

    .

--- ejer2.py
+++ ejer2.py
@@ -1,7 +1,6 @@
 import re
 pattern= "\\b(E[-]?)?(\d{4})(\s|\-|)([A-Z]{3})\\b"
 result=re.findall(pattern, input())
-print(result)
+s=""
 for i in result:
-    s=""
     print(s.join(i))
--- ejer3.py
+++ ejer3.py
@@ -2,4 +2,4 @@ import re
 pattern="(.*)\\b(\d{4})-(\d{2})-(\d{2})\\b(.*)"
 result=re.findall(pattern, input())
 if result:
-    print(result[0][0]+result[0][3]+"-"+result[0][2]+"-"+result[0][1]+result[0][4])
+    print(result[0][0]+result[0][3]+"."+result[0][2]+"."+result[0][1]+result[0][4])
