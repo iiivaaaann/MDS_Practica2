731b76f05d15f90792dc5bffbd81ef810af1b547
i.garcial.2020 <i.garcial.2020@alumnos.urjc.es>
2023-04-11 13:55:04 +0200

    Pasan TODOS los casos de prueba del juez.

--- ejer5.py
+++ ejer5.py
@@ -1,5 +1,5 @@
 import re
-pattern="\\b(?:C\/|Calle) (\\b[A-ZÑÁÉÍÓÚ]{1}[a-zñíéáóüú]+\\b)(?:,)?(?:\s+)(?:N|n)?(?:º\s*)?(\d+)(?:,)(?:\s+)(\d{5})\\b"
+pattern="\\b(?:C\/|Calle) (\\b[A-ZÑÁÉÍÓÚ]{1}[a-zñíéáóüú]+\\b)(?:,)?(?:\s*)(?:Nº|nº|n|N)?\s*(\d+),\s*(\d{5})\\b"
 result=re.findall(pattern, input())
 for r in result:
     print(r[2]+"-"+r[0]+"-"+r[1])
